c7a877654ed3f06da40a1d630c04aa99292b04df
Matthew Haworth <matthewh2008@googlemail.com>
2014-01-16 01:05:21 +0000

    Added problem 1

---
+++ Problem1/solution.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int total = 0;
+    for (int i = 0; i < 1000; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            total += i;
+        }
+    }
+
+    printf("Total: %d\n", total);    
+}
